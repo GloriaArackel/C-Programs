a11f22029b1fa078909a0734ca4130f08f985f51
GloriaArackel <163562151+GloriaArackel@users.noreply.github.com>
2024-04-11 06:25:52 -0700

    Add files via upload

---
+++ Area&Circuference.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int r;
+float area,circumference;
+printf("Enter the radius");
+scanf("%d",&r);
+area=3.14*r*r;
+circumference=2*3.14*r;
+printf("Area of the cicle=%d",area);
+printf("Circumference of the circle=%d",circuference);
+]
