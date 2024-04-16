ed4c15448ec1387ed39be51438d524968300aafc
GloriaArackel <163562151+GloriaArackel@users.noreply.github.com>
2024-04-16 07:50:19 -0700

    Add files via upload

---
+++ Sum and Average of elements in an array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+float avg;
+int sum=0;
+int n,i;
+printf("Enter the no.of elements");
+for(i=o;i<n;i++)
+{
+printf("a(%d)",i);
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+avg=sum/n;
+}
+}
